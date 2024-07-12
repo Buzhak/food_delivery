14dace737f64e3a6c38f47d62f0dc1aa0a45cab5
Buzhak <byzhak@ya.ru>
2024-07-12 17:12:51 +0300

    add menu class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const data = {
+    1: {
+        img: 'img/tabs/vegy.jpg',
+        title: 'Меню "Фитнес"',
+        text: 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        price: 100
+    },
+    2: {
+        img: 'img/tabs/elite.jpg',
+        title: 'Меню “Премиум”',
+        text: 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан! Заказывайте прямо сейчас!',
+        price: 350
+    },
+    3: {
+        img: 'img/tabs/post.jpg',
+        title: 'Меню "Постное"',
+        text: 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        price: 150
+    },
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
@@ -153,4 +174,54 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     window.addEventListener('scroll', showModelByScrol);
+
+
+    // Menu items
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 90;
+            this.changeToRouble();
+        }
+
+        changeToRouble() {
+            this.price = +this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    // const menuItems = document.querySelectorAll('.menu .container .menu__item');
+    // menuItems.forEach(i => {i.remove()});
+
+    new MenuCard(
+        'img/tabs/vegy.jpg',
+        'vegy',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        5,
+        '.menu .container'
+    ).render();
+    
 });
