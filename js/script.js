c1f9d771b407a9dc5c0c93415b24eb1a6adbd5b4
Buzhak <byzhak@ya.ru>
2024-07-19 15:55:55 +0300

    fix calculator

--- js/script.js
+++ js/script.js
@@ -499,11 +499,45 @@ document.addEventListener('DOMContentLoaded', () => {
     // Calculator
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female',
+
+    let sex,
         height,
         weight,
         age,
+        ratio;
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex')
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio')
+    } else {
         ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
+
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            }
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+            
+        });
+    }
+
+    initLocalSettings('.calculating__choose-item', 'calculating__choose-item_active');
+
+    
+
 
     function calcTotal() {
         if(!sex || !height || !weight || !age || !ratio) {
@@ -528,8 +562,10 @@ document.addEventListener('DOMContentLoaded', () => {
             if(target && target.classList.contains('calculating__choose-item')) {
                 if(e.target.getAttribute('data-ratio')) {
                     ratio = e.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', ratio)
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', sex)
                 }
 
                 elements.forEach(elem => {
@@ -548,6 +584,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+            if (input.value.match(/\D/g)) {
+                input.style.border = '1px solid red'
+            } else {
+                input.style.border = 'none'
+            }
+
             switch(input.getAttribute('id')) {
                 case 'height':
                     height = +input.value;
